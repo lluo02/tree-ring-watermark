e045b879aa791500834a7aa3f5acd0fcb8e1c4d4
Shilin Huang <shilin40@nexuscml00.umiacs.umd.edu>
2023-12-16 01:17:41 -0500

    add tests

--- tests/test_pattern_prop.py
+++ tests/test_pattern_prop.py
@@ -4,28 +4,16 @@ import copy
 from tqdm import tqdm
 from statistics import mean, stdev
 from sklearn import metrics
-
+import sys
+import os
 import torch
-
+project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_path)
 from inverse_stable_diffusion import InversableStableDiffusionPipeline
 from diffusers import DPMSolverMultistepScheduler
 import open_clip
 from optim_utils import *
 from io_utils import *
-positive = True
-tol = 0.01
-def alt(x):
-    global positive
-    if positive:
-        positive = not positive 
-        if x < 0:
-            return -x
-        return x
-    else:
-        positive = not positive 
-        if x > 0:
-            return -x
-        return x
 
 if __name__ == '__main__':
   device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -40,7 +28,7 @@ if __name__ == '__main__':
   pipe = pipe.to(device)
 
   init_latents_w = pipe.get_random_latents()
-  init_latents_w.apply_(lambda x: alt(x))
-  print(init_latents_w)
+  
 
-
+    
+  no_w_metric, w_metric = eval_watermark(reversed_latents_no_w, reversed_latents_w, watermarking_mask, gt_patch, args)
